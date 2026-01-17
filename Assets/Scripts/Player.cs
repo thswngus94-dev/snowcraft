a3ba87ca9d7ae8ba530ac9fc51e1112089896fd3
SonJuhyun <thswngus94@gmail.com>
2026-01-17 19:15:16 +0900

    feat : 플레이어 눈 던지기 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,20 @@ public class Player : MonoBehaviour
 
     public GameObject snowballPrefab;
 
+    private float chargeTime = 0f;
+    public float maxForce = 30f;
+    public float chargeSpeed = 10f;
+
+    
+
     void OnMouseDown()
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10f;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-
         offset = transform.position - worldPos;
+
+        chargeTime = 0f;
 
     }
 
@@ -21,27 +28,29 @@ public class Player : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10f;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-
         transform.position = worldPos + offset;
+
+        chargeTime += Time.deltaTime;
     }
 
-    //void Update()
-    //{
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Fire();
-        //}
+    void OnMouseUp()
+    {
+        Fire();
+    }
 
-    //}
 
-    //void Fire()
-    //{
-    //    GameObject snowball = Instantiate(snowballPrefab, transform.position, Quaternion.identity);
-    //    Rigidbody2D ballRigidbody2D = snowball.GetComponent<Rigidbody2D>();
+    void Fire()
+    {
+        Vector3 spawnPos = transform.position + new Vector3(-1f, 0, 0);
+        GameObject snowball = Instantiate(snowballPrefab, spawnPos, Quaternion.identity);
+        Rigidbody2D ballRigidbody2D = snowball.GetComponent<Rigidbody2D>();
 
-    //    ballRigidbody2D.AddForce(new Vector2(1, 1) * 10f, ForceMode2D.Impulse);
+        float finalForce = Mathf.Min(chargeTime * chargeSpeed, maxForce);
 
-    //}
+        // 이 부분 따로 정리해보기
+        ballRigidbody2D.AddForce(new Vector2(-1, 0) * 10f, ForceMode2D.Impulse);
+
+    }
 
 
 
